1f50bb250edeffb19fd4b1aec29648b99be2caf4
miaardi <127352524+miaardi@users.noreply.github.com>
2023-06-22 19:11:37 -0300

    hh

--- app.js
+++ app.js
@@ -1,21 +1,67 @@
+// Variables for cart functionality
+const cartButton = document.getElementById('cart-button');
+const cartContent = document.getElementById('cart-content');
+const cartItems = document.getElementById('cart-items');
+const cartTotal = document.getElementById('cart-total');
+let cart = [];
+
 // Function to handle cart button click
 function handleCartButtonClick() {
-    const header = document.querySelector("#cart-button");
-const contenedor = document.querySelector("#contenedor");
-const body = document.querySelector("body");
-window.addEventListener("scroll", function(){
-    if(contenedor.getBoundingClientRect().top<10){
-        header.classList.add("scroll")
-    }
-    else{
-        header.classList.remove("scroll")
-    }
-})
-    // Add your cart functionality here
-    // For example, show a modal or navigate to the cart page
-    console.log('Cart button clicked!');
- }
- 
- // Event listener for cart button
- const cartButton = document.getElementById('cart-button');
- cartButton.addEventListener('click', handleCartButtonClick);
+   // Toggle cart content visibility
+   if (cartContent.style.display === 'block') {
+      cartContent.style.display = 'none';
+   } else {
+      cartContent.style.display = 'block';
+      renderCart();
+   }
+}
+
+// Function to render the cart content
+function renderCart() {
+   // Clear previous cart items
+   cartItems.innerHTML = '';
+
+   if (cart.length === 0) {
+      // Display "carrito vacío" message
+      const emptyCartMessage = document.createElement('li');
+      emptyCartMessage.textContent = 'Carrito vacío';
+      cartItems.appendChild(emptyCartMessage);
+
+      // Clear cart total
+      cartTotal.textContent = '';
+   } else {
+      // Render each item in the cart
+      cart.forEach(item => {
+         const cartItem = document.createElement('li');
+         cartItem.textContent = item.name;
+         cartItems.appendChild(cartItem);
+      });
+
+      // Calculate and display cart total
+      const total = cart.reduce((acc, item) => acc + item.price, 0);
+      cartTotal.textContent = `Total: $${total}`;
+   }
+}
+
+// Function to handle "Comprar" button click
+function handleBuyButtonClick(name, price) {
+   // Add item to the cart
+   cart.push({ name, price });
+
+   // Render the updated cart
+   renderCart();
+}
+
+// Event listeners
+cartButton.addEventListener('click', handleCartButtonClick);
+
+// Example "Comprar" buttons for products
+const buyButtons = document.getElementsByClassName('buy-button');
+for (let i = 0; i < buyButtons.length; i++) {
+   const button = buyButtons[i];
+   button.addEventListener('click', function() {
+      const itemName = this.dataset.name;
+      const itemPrice = parseFloat(this.dataset.price);
+      handleBuyButtonClick(itemName, itemPrice);
+   });
+}
