e27bbcc73989c9125bbaa483ce40a48d4de33c4b
miaardi <127352524+miaardi@users.noreply.github.com>
2023-06-22 19:03:49 -0300

    ss

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // Function to handle cart button click
 function handleCartButtonClick() {
+    const header = document.querySelector("#header");
+const contenedor = document.querySelector("#contenedor");
+const body = document.querySelector("body");
+window.addEventListener("scroll", function(){
+    if(contenedor.getBoundingClientRect().top<10){
+        header.classList.add("scroll")
+    }
+    else{
+        header.classList.remove("scroll")
+    }
+})
     // Add your cart functionality here
     // For example, show a modal or navigate to the cart page
     console.log('Cart button clicked!');
